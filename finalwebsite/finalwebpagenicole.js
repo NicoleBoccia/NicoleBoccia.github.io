cb82cad4fbf62c37b117436fc1514cf68b94a2d7
Nicole Boccia <nicole.boccia@hotmail.com>
2015-12-12 17:23:27 -0500

    final page, website java second com

--- finalwebsite/finalwebpagenicole.js
+++ finalwebsite/finalwebpagenicole.js
@@ -6,13 +6,13 @@ document.write("<br />" + month + "/" + date + "/" + year);
 
 
 		setTimeout("document.bgColor='grey'", 1000)
-		setTimeout("document.bgColor='lightpink'", 1500)
-		setTimeout("document.bgColor = 'pink'", 2000)
+		setTimeout("document.bgColor='whie'", 1500)
+		setTimeout("document.bgColor = 'grey'", 2000)
 		setTimeout("document.bgColor =  'white'", 2500)
 		setTimeout("document.bgColor = 'aqua'", 3000)
 		setTimeout("document.bgColor = 'white'", 3500)
 		setTimeout("document.bgColor = 'orange'", 4000)
-		setTimeout("document.bgColor = 'blue'", 4500)
+		setTimeout("document.bgColor = 'teal'", 4500)
 		setTimeout("document.bgColor = 'white'", 5000)
 		setTimeout("document.bgColor = 'grey'", 5500)
 		setTimeout("document.bgColor = 'white'", 6000)
