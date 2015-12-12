1177b07940c2b3356426d2c27e01d7c3d79e64d6
Nicole Boccia <nicole.boccia@hotmail.com>
2015-12-12 16:16:16 -0500

    js commit first

---
+++ finalwebsite/finalwebpagenicole.js
@@ -0,0 +1,18 @@
+var todayDate = new Date();
+var month = todayDate.getMonth();
+var year = todayDate.getFullYear();
+var date = todayDate.getDate();
+document.write("<br />" + month + "/" + date + "/" + year);
+
+
+		setTimeout("document.bgColor='grey'", 1000)
+		setTimeout("document.bgColor='lightpink'", 1500)
+		setTimeout("document.bgColor = 'pink'", 2000)
+		setTimeout("document.bgColor =  'white'", 2500)
+		setTimeout("document.bgColor = 'aqua'", 3000)
+		setTimeout("document.bgColor = 'white'", 3500)
+		setTimeout("document.bgColor = 'orange'", 4000)
+		setTimeout("document.bgColor = 'blue'", 4500)
+		setTimeout("document.bgColor = 'white'", 5000)
+		setTimeout("document.bgColor = 'grey'", 5500)
+		setTimeout("document.bgColor = 'white'", 6000)
